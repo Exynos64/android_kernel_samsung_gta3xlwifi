8738ce0aede7411ce1b910e4d2b932b25ba7ace0
Maxim Leo <mxsleo@outlook.com>
2024-07-11 14:41:21 +0300

    build_kernel.sh: remove duplicating parameter

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,5 @@ export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
 
-make ARCH=arm64 exynos7885-gta3xlwifi_defconfig
-make ARCH=arm64 -j64
+make exynos7885-gta3xlwifi_defconfig
+make -j64
